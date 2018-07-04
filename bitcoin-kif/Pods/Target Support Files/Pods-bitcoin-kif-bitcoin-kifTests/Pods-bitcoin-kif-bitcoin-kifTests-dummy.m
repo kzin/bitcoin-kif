0745081deed42c1f7e4a6620bfe4315aecfac36a
Rodrigo Cavalcante <rodrigo.cavalcante@concrete.com.br>
2018-07-04 10:43:32 -0300

    install test frameworks

---
+++ bitcoin-kif/Pods/Target Support Files/Pods-bitcoin-kif-bitcoin-kifTests/Pods-bitcoin-kif-bitcoin-kifTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bitcoin_kif_bitcoin_kifTests : NSObject
+@end
+@implementation PodsDummy_Pods_bitcoin_kif_bitcoin_kifTests
+@end
---
+++ bitcoin-kif/bitcoin-kifTests/spec/HistoryPortraitViewSpec.swift
@@ -0,0 +1,26 @@
+//
+//  HistoryPortraitViewSpec.swift
+//  bitcoin-kifTests
+//
+//  Created by r.vieira.cavalcante on 04/07/18.
+//  Copyright © 2018 r.vieira.cavalcante. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+import KIF_Quick
+@testable import bitcoin_kif
+
+
+class HistoryPorstraitViewSpec: KIFSpec {
+    override func spec() {
+
+        context("default layout") {
+            
+            it("") {
+                expect(true) == true
+            }
+        }
+    }
+}
